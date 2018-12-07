1f0b2631db836301fe41ca9d7c12710f4b5feed5
Maryam Tasviri <tasviri.maryam@yahoo.com>
2018-12-07 05:01:57 +0330

    session-7-1-2 hm

--- app/src/main/java/com/example/malam/myapplication/MainActivity.java
+++ app/src/main/java/com/example/malam/myapplication/MainActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +16,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("yessss!!");
+
+        Button button = findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, SecondActivity.class));
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                EditText name, age, email, phone;
+                name = findViewById(R.id.name);
+                age = findViewById(R.id.age);
+                email = findViewById(R.id.email);
+                phone = findViewById(R.id.phone);
+
+                intent.putExtra("Name",name.getText().toString() );
+                intent.putExtra("Age",age.getText().toString() );
+                intent.putExtra("Email",email.getText().toString() );
+                intent.putExtra("Phone",phone.getText().toString() );
+
+                startActivity(intent);
+
+            }
+        });
     }
 
 
-    Button button = findViewById(R.id.btn);
 
 
 }
--- app/src/main/java/com/example/malam/myapplication/third_activity.java
+++ app/src/main/java/com/example/malam/myapplication/third_activity.java
@@ -1,7 +1,9 @@
 package com.example.malam.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class third_activity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class third_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_activity);
+
+        TextView textViewN = findViewById(R.id.name);
+        TextView textViewA = findViewById(R.id.age);
+        TextView textViewE = findViewById(R.id.email);
+        TextView textViewP = findViewById(R.id.phone);
+
+        Intent intent = getIntent();
+        String name, age, email, phone;
+        name = intent.getStringExtra("Name");
+        age = intent.getStringExtra("Age");
+        email = intent.getStringExtra("Email");
+        phone = intent.getStringExtra("Phone");
+
+        textViewN.setText(name);
+        textViewA.setText(age);
+        textViewE.setText(email);
+        textViewP.setText(phone);
+
+
+
+
     }
 }
